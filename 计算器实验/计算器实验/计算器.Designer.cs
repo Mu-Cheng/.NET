5d0c46a65e6f788dfb2c1c29825edc0ef655ef26
MuChen <752572065@qq.com>
2016-09-13 22:38:07 +0800

    自写win应用计算器

---
+++ 计算器实验/计算器实验/计算器.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace 计算器实验
+{
+    partial class 计算器
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.SubBtn = new System.Windows.Forms.Button();
+            this.MulBtn = new System.Windows.Forms.Button();
+            this.EqurBtn = new System.Windows.Forms.Button();
+            this.PointBtn = new System.Windows.Forms.Button();
+            this.DivBtn = new System.Windows.Forms.Button();
+            this.LeftBrackets = new System.Windows.Forms.Button();
+            this.RightBrackets = new System.Windows.Forms.Button();
+            this.BreakOneBtn = new System.Windows.Forms.Button();
+            this.BreakBtn = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Font = new System.Drawing.Font("宋体", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.InputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(331, 53);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(4, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(85, 176);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(167, 175);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(4, 206);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(86, 206);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(168, 205);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(4, 236);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(75, 23);
+            this.button0.TabIndex = 10;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(249, 146);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBtn.TabIndex = 11;
+            this.AddBtn.Text = "+";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // SubBtn
+            // 
+            this.SubBtn.Location = new System.Drawing.Point(249, 175);
+            this.SubBtn.Name = "SubBtn";
+            this.SubBtn.Size = new System.Drawing.Size(75, 23);
+            this.SubBtn.TabIndex = 12;
+            this.SubBtn.Text = "-";
+            this.SubBtn.UseVisualStyleBackColor = true;
+            this.SubBtn.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // MulBtn
+            // 
+            this.MulBtn.Location = new System.Drawing.Point(249, 205);
+            this.MulBtn.Name = "MulBtn";
+            this.MulBtn.Size = new System.Drawing.Size(75, 23);
+            this.MulBtn.TabIndex = 13;
+            this.MulBtn.Text = "*";
+            this.MulBtn.UseVisualStyleBackColor = true;
+            this.MulBtn.Click += new System.EventHandler(this.MulBtn_Click);
+            // 
+            // EqurBtn
+            // 
+            this.EqurBtn.Location = new System.Drawing.Point(249, 236);
+            this.EqurBtn.Name = "EqurBtn";
+            this.EqurBtn.Size = new System.Drawing.Size(75, 23);
+            this.EqurBtn.TabIndex = 14;
+            this.EqurBtn.Text = "=";
+            this.EqurBtn.UseVisualStyleBackColor = true;
+            this.EqurBtn.Click += new System.EventHandler(this.EqurBtn_Click);
+            // 
+            // PointBtn
+            // 
+            this.PointBtn.Location = new System.Drawing.Point(86, 236);
+            this.PointBtn.Name = "PointBtn";
+            this.PointBtn.Size = new System.Drawing.Size(75, 23);
+            this.PointBtn.TabIndex = 15;
+            this.PointBtn.Text = ".";
+            this.PointBtn.UseVisualStyleBackColor = true;
+            this.PointBtn.Click += new System.EventHandler(this.PointBtn_Click);
+            // 
+            // DivBtn
+            // 
+            this.DivBtn.Location = new System.Drawing.Point(168, 235);
+            this.DivBtn.Name = "DivBtn";
+            this.DivBtn.Size = new System.Drawing.Size(75, 23);
+            this.DivBtn.TabIndex = 16;
+            this.DivBtn.Text = "/";
+            this.DivBtn.UseVisualStyleBackColor = true;
+            this.DivBtn.Click += new System.EventHandler(this.DivBtn_Click);
+            // 
+            // LeftBrackets
+            // 
+            this.LeftBrackets.Location = new System.Drawing.Point(4, 118);
+            this.LeftBrackets.Name = "LeftBrackets";
+            this.LeftBrackets.Size = new System.Drawing.Size(75, 23);
+            this.LeftBrackets.TabIndex = 17;
+            this.LeftBrackets.Text = "(";
+            this.LeftBrackets.UseVisualStyleBackColor = true;
+            this.LeftBrackets.Click += new System.EventHandler(this.LeftBrackets_Click);
+            // 
+            // RightBrackets
+            // 
+            this.RightBrackets.Location = new System.Drawing.Point(86, 118);
+            this.RightBrackets.Name = "RightBrackets";
+            this.RightBrackets.Size = new System.Drawing.Size(75, 23);
+            this.RightBrackets.TabIndex = 18;
+            this.RightBrackets.Text = ")";
+            this.RightBrackets.UseVisualStyleBackColor = true;
+            this.RightBrackets.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // BreakOneBtn
+            // 
+            this.BreakOneBtn.Location = new System.Drawing.Point(168, 117);
+            this.BreakOneBtn.Name = "BreakOneBtn";
+            this.BreakOneBtn.Size = new System.Drawing.Size(75, 23);
+            this.BreakOneBtn.TabIndex = 19;
+            this.BreakOneBtn.Text = "退格";
+            this.BreakOneBtn.UseVisualStyleBackColor = true;
+            this.BreakOneBtn.Click += new System.EventHandler(this.BreakOneBtn_Click);
+            // 
+            // BreakBtn
+            // 
+            this.BreakBtn.Location = new System.Drawing.Point(249, 118);
+            this.BreakBtn.Name = "BreakBtn";
+            this.BreakBtn.Size = new System.Drawing.Size(75, 23);
+            this.BreakBtn.TabIndex = 20;
+            this.BreakBtn.Text = "清屏";
+            this.BreakBtn.UseVisualStyleBackColor = true;
+            this.BreakBtn.Click += new System.EventHandler(this.BreakBtn_Click);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Enabled = false;
+            this.OutputTextBox.Font = new System.Drawing.Font("宋体", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.OutputTextBox.Location = new System.Drawing.Point(0, 59);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(331, 52);
+            this.OutputTextBox.TabIndex = 21;
+            this.OutputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.OutputTextBox.TextChanged += new System.EventHandler(this.OutputTextBox_TextChanged);
+            // 
+            // 计算器
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 271);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.BreakBtn);
+            this.Controls.Add(this.BreakOneBtn);
+            this.Controls.Add(this.RightBrackets);
+            this.Controls.Add(this.LeftBrackets);
+            this.Controls.Add(this.DivBtn);
+            this.Controls.Add(this.PointBtn);
+            this.Controls.Add(this.EqurBtn);
+            this.Controls.Add(this.MulBtn);
+            this.Controls.Add(this.SubBtn);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "计算器";
+            this.Text = "计算器";
+            this.Load += new System.EventHandler(this.计算器_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button SubBtn;
+        private System.Windows.Forms.Button MulBtn;
+        private System.Windows.Forms.Button EqurBtn;
+        private System.Windows.Forms.Button PointBtn;
+        private System.Windows.Forms.Button DivBtn;
+        private System.Windows.Forms.Button LeftBrackets;
+        private System.Windows.Forms.Button RightBrackets;
+        private System.Windows.Forms.Button BreakOneBtn;
+        private System.Windows.Forms.Button BreakBtn;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
---
+++ 计算器实验/计算器实验/计算器.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 计算器实验
+{
+    public partial class 计算器 : Form
+    {
+        public 计算器()
+        {
+            InitializeComponent();
+        }
+
+        private void 计算器_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void EqurBtn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            OutputTextBox.Text = calculation(InputTextBox.Text);
+        }
+
+        private string calculation(string text)
+        {
+            //throw new NotImplementedException();
+            Stack st = new Stack();
+            Queue ans = new Queue();
+
+            while (text != "")
+            {
+                int len = getNumOfLen(text);
+                string s1 = text.Substring(0,len);
+                
+                switch (s1)
+                {
+                    case "+":
+                        if (st.Count == 0)
+                        {
+                            st.Push("+");
+                        }else
+                        {
+                            while (st.Count != 0)
+                            {
+                                string op = (string)st.Peek();
+                                if (op == "(")
+                                    break;
+                                ans.Enqueue(op);
+                                st.Pop();
+                            }
+
+                            st.Push(s1);
+                        }
+                       
+                        break;
+                    case "-":
+                        if (st.Count == 0)
+                        {
+                            st.Push("-");
+                        }
+                        else
+                        {
+                            while (st.Count != 0)
+                            {
+                                string op = (string)st.Peek();
+                                if (op == "(")
+                                    break;
+                                ans.Enqueue(op);
+                                st.Pop();
+                            }
+                            st.Push(s1);
+                        }
+
+
+
+                        break;
+                    case "*":
+                        if (st.Count == 0)
+                        {
+                            st.Push("*");
+                        }
+                        else
+                        {
+                            while (st.Count != 0)
+                            {
+                                string op = (string)st.Peek();
+                                if (op == "+" || op == "-")
+                                    break;
+                                if (op == "(")
+                                    break;
+                                ans.Enqueue(op);
+                                st.Pop();
+                            }
+                            st.Push(s1);
+                        }
+                        break;
+                    case "/":
+                        if (st.Count == 0)
+                        {
+                            st.Push("/");
+                        }
+                        else
+                        {
+                            while (st.Count != 0)
+                            {
+                                string op = (string)st.Peek();
+                                if (op == "+" || op == "-")
+                                    break;
+                                if (op == "(")
+                                    break;
+                                ans.Enqueue(op);
+                                st.Pop();
+                            }
+                            st.Push(s1);
+                        }
+                        break;
+                    case "(":
+                        st.Push("(");
+                        break;
+                    case ")":
+                        while (st.Count != 0)
+                        {
+                            string op = (string)st.Pop();
+                            if (op == "(")
+                                break;
+                            ans.Enqueue(op);
+                        
+                        }
+                        break;
+                    default:
+                        ans.Enqueue(s1);
+                        break;
+                }
+
+                text = text.Substring(len);
+            }
+
+            while (st.Count != 0)
+            {
+                string op = (string)st.Pop();
+                ans.Enqueue(op);
+            }
+            string op1, op2, result;
+            double a, b, c;
+            while (ans.Count != 0)
+            {
+                string s = (string)ans.Dequeue();
+
+                switch (s)
+                {
+                    case "+":
+                        op1 = (string)st.Pop();
+                        op2 = (string)st.Pop();
+                        a = Convert.ToDouble(op1);
+                        b = Convert.ToDouble(op2);
+                        c = a + b;
+                        result = c.ToString();
+
+                        st.Push(result);
+                        break;
+                    case "-":
+                        op1 = (string)st.Pop();
+                        op2 = (string)st.Pop();
+                        a = Convert.ToDouble(op1);
+                        b = Convert.ToDouble(op2);
+                        c = b - a;
+                        result = c.ToString();
+
+                        st.Push(result);
+                        break;
+                    case "*":
+                        op1 = (string)st.Pop();
+                        op2 = (string)st.Pop();
+                        a = Convert.ToDouble(op1);
+                        b = Convert.ToDouble(op2);
+                        c = a * b;
+                        result = c.ToString();
+
+                        st.Push(result);
+                        break;
+                    case "/":
+                        op1 = (string)st.Pop();
+                        op2 = (string)st.Pop();
+                        a = Convert.ToDouble(op1);
+                        b = Convert.ToDouble(op2);
+                        if (Math.Abs(a)<1e-8)
+                        {
+                            return "除0错误";
+                        }
+                        c = b / a;
+                        result = c.ToString();
+
+                        st.Push(result);
+                        break;
+                    default:
+                        st.Push(s);
+                        break;
+                }
+
+
+            }
+
+            return (string)st.Pop();
+        }
+
+        private int getNumOfLen(string text)
+        {
+            //throw new NotImplementedException();
+            int i;
+            int ans = 0;
+            int len = text.Length;
+            char[] s = text.ToCharArray();
+            for(i = 0; i < len; i++)
+            {
+                if ('0' <= s[i] && s[i] <= '9'||s[i]=='.')
+                    ans++;
+                else
+                {
+                    if(ans == 0)
+                    {
+                        ans++;
+    
+                    }
+                    break;
+                }
+            }
+            return ans;
+        }
+
+        private void BreakBtn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text = "";
+            OutputTextBox.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void MulBtn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (InputTextBox.Text == "") return;
+            int len = getNumOfLen(InputTextBox.Text);
+            string s = InputTextBox.Text.Substring(0, len);
+            if (s == "0") return;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void PointBtn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void DivBtn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void LeftBrackets_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            InputTextBox.Text += btn.Text;
+        }
+
+        private void BreakOneBtn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if(InputTextBox.Text!="")
+                InputTextBox.Text = InputTextBox.Text.Substring(0, InputTextBox.Text.Length-1);
+           
+        }
+
+        private void OutputTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
